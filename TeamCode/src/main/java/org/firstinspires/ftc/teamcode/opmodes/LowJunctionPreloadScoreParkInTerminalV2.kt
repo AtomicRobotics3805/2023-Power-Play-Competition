d79c4de9e86ed79855b4505f56628c3cfab53041
REM-Codes <rowan.mcalpin@gmail.com>
2022-10-22 11:21:24 -0700

    Forgot to do something aelse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LowJunctionPreloadScoreParkInTerminalV2.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LowJunctionPreloadScoreParkInTerminalV2.kt
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode
+package org.firstinspires.ftc.teamcode.opmodes
 
 import com.atomicrobotics.cflib.Constants
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
@@ -7,6 +7,7 @@ import org.firstinspires.ftc.teamcode.drive.CompetitionMecanumDriveConstants
 import com.atomicrobotics.cflib.driving.drivers.MecanumDrive
 import com.atomicrobotics.cflib.driving.localizers.TwoWheelOdometryLocalizer
 import org.firstinspires.ftc.teamcode.localizers.CompetitionOdometryConstants
+import org.firstinspires.ftc.teamcode.routines.Routines
 import org.firstinspires.ftc.teamcode.trajectoryFactory.CompetitionTrajectoryFactory
 
 @Autonomous(name = "Score Preload in Low Junction & Park in Terminal")
