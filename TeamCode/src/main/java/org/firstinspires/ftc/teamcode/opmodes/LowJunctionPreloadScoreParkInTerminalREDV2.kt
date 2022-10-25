a57a6b7311924f9131c589357146b5390690898b
REMCodes <rowan.mcalpin@gmail.com>
2022-10-25 09:55:21 -0700

    Still broken..

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LowJunctionPreloadScoreParkInTerminalREDV2.kt
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmodes
+
+import com.atomicrobotics.cflib.Constants
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.atomicrobotics.cflib.opmodes.AutonomousOpMode
+import org.firstinspires.ftc.teamcode.drive.CompetitionMecanumDriveConstants
+import com.atomicrobotics.cflib.driving.drivers.MecanumDrive
+import com.atomicrobotics.cflib.driving.localizers.TwoWheelOdometryLocalizer
+import org.firstinspires.ftc.teamcode.localizers.CompetitionOdometryConstants
+import org.firstinspires.ftc.teamcode.routines.Routines
+import org.firstinspires.ftc.teamcode.trajectoryFactory.CompetitionTrajectoryFactory
+
+@Autonomous(name = "Score Preload in Low Junction & Park in Terminal")
+class LowJunctionPreloadScoreParkInTerminalREDV2 : AutonomousOpMode(
+    Constants.Color.BLUE,
+    CompetitionTrajectoryFactory,
+    { Routines.leftMainRoutine },
+    { Routines.initializationRoutine },
+    MecanumDrive(
+        CompetitionMecanumDriveConstants,
+        TwoWheelOdometryLocalizer(CompetitionOdometryConstants())
+    ) { CompetitionTrajectoryFactory.leftStartPose }
+)
